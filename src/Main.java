4e01cc855938c9f28200bfdf020421ec00ee4d86
Ilya Valikov <valikov.skeleton1977@yandex.ru>
2023-04-03 18:45:09 +0300

    add Project2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13000;
+        int minValue = 20;
+        int bonus = amount / minValue;
+
+        System.out.println(bonus);
+
+
+    }
+}
